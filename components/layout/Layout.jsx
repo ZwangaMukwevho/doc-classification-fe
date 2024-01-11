d5eeba87fc1420b5f889d368cbcc658bb2465db8
ZwangaMukwevho <zwangatm3@gmail.com>
2024-01-11 23:54:29 +0200

    fix: added a centred div

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,12 @@
+// Layout.jsx
+
+const Layout = ({ children }) => {
+    return (
+      <div className="mx-40">
+        {children}
+      </div>
+    );
+  };
+  
+  export default Layout;
+  
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import NavBar from '@/components/navbar'
+import NavBar from '@/components/Navbar'
+import Layout from '@/components/layout/Layout'
+import { WelcomeCard } from '@/components/home/WelcomeCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +10,9 @@ export default function Home() {
   return (
     <main className="dark text-foreground bg-background">
       <NavBar></NavBar>
-
+      <Layout>
+        <WelcomeCard/>
+      </Layout>
     </main>
   )
 }
