afe2cf29a55a1e68a4b4d62d4659b80ead1a740f
ZwangaMukwevho <zwangatm3@gmail.com>
2024-01-19 00:11:04 +0200

    fix: updated gmail and gdrive

--- components/forms/SignInCard.jsx
+++ components/forms/SignInCard.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";
 
-export const SignInCard = () => {
+export const SignInCard = ({service,imgSrc}) => {
     return (
-      <Card shadow="sm" isPressable onPress={() => console.log("item pressed")}>
-        <CardBody className="overflow-visible p-0">
-          <Image
-            shadow="sm"
-            radius="lg"
-            width="100%"
-            alt={'sign in'}
-            className="w-full object-cover h-[140px]"
-            src={'public/vercel.svg'}
-          />
-        </CardBody>
-        <CardFooter className="text-small justify-between">
-          <b>{'Sign to Gmail'}</b>
-          <p className="text-default-500">{'R29'}</p>
-        </CardFooter>
-      </Card>
+        <Card isFooterBlurred isPressable className="w-3/4 h-3/4 col-span-12 sm:col-span-5">
+            <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">Sign In</p>
+                <h4 className="text-black font-medium text-2xl">{service}</h4>
+            </CardHeader>
+            <Image
+                removeWrapper
+                alt="Card example background"
+                className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                src={imgSrc}
+                />
+            <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+            <div>
+                <p className="text-black text-tiny">Click to sign in.</p>
+            </div>
+            {/* <Button className="text-tiny" color="primary" radius="full" size="sm">
+                Sign in
+            </Button> */}
+            </CardFooter>
+        </Card>
     );
     
 }
--- pages/signin/index.js
+++ pages/signin/index.js
@@ -2,7 +2,7 @@ import { SignInCard } from "@/components/forms/SignInCard"
 
 export default function Signin() {
   return (
-    <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center">
         <SignInCard/>
     </div>
     
